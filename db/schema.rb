b7648a9c211ba9a90a3ebccb29943430d35bc96e
Tomu Komatsu <tomu5642514@gmail.com>
2022-01-25 16:09:57 -0500

    Add Bookings and Cars Controllers

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2022_01_25_193448) do
+ActiveRecord::Schema.define(version: 2022_01_25_200559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookings", force: :cascade do |t|
+    t.boolean "is_accepted"
+    t.date "check_in"
+    t.date "check_out"
+    t.decimal "price_per_day"
+    t.decimal "amount"
+    t.integer "days"
+    t.bigint "user_id", null: false
+    t.bigint "car_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["car_id"], name: "index_bookings_on_car_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "cars", force: :cascade do |t|
     t.string "brand"
@@ -31,11 +44,18 @@ ActiveRecord::Schema.define(version: 2022_01_25_193448) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_cars_on_user_id"
   end
+
   create_table "reviews", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
     t.integer "rating"
     t.text "comment"
+    t.bigint "user_id", null: false
+    t.bigint "booking_id", null: false
+    t.bigint "car_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
+    t.index ["car_id"], name: "index_reviews_on_car_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -56,5 +76,10 @@ ActiveRecord::Schema.define(version: 2022_01_25_193448) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "cars"
+  add_foreign_key "bookings", "users"
   add_foreign_key "cars", "users"
+  add_foreign_key "reviews", "bookings"
+  add_foreign_key "reviews", "cars"
+  add_foreign_key "reviews", "users"
 end
