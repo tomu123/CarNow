184487d61f025a8c48400c09f9601a9cd8fbaac4
cristian trinidad <cristiantrinidadph@gmail.com>
2022-01-25 15:08:49 -0500

    Cambios

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,11 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :car
+
+  validates :is_accepted, precense: true
+  validates :check_in, precense: true
+  validates :check_out, precense: true
+  validates :price_per_day, precense: true
+  validates :amount, precense: true
+  validates :days, precense: true
 end
